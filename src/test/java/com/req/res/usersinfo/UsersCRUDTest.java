e4b61455fa2d2893673b2ecbf7ec071a3a682bef
Ankita1986 <ankitakaswala1@gmail.com>
2020-05-20 11:40:01 +0100

    reqres-serenity-project

---
+++ src/test/java/com/req/res/usersinfo/UsersCRUDTest.java
@@ -0,0 +1,100 @@
+package com.req.res.usersinfo;
+
+import com.req.res.model.UsersPojo;
+import com.req.res.testbase.TestBase;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Title;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import static org.hamcrest.Matchers.*;
+
+
+/**
+ * Created by Ankita
+ */
+@RunWith(SerenityRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class UsersCRUDTest extends TestBase {
+
+    static String firstName;
+    static String job;
+
+    @Title("Create an User Successfully and verify name added")
+    @Test
+    public void test001() {
+
+        UsersPojo usersPojo = new UsersPojo();
+        usersPojo.setName(firstName = "Mary");
+        usersPojo.setJob(job = "Leader");
+
+        SerenityRest.rest().given()
+                .log().all()
+                .header("Content-Type", "application/json")
+                .when()
+                .body(usersPojo)
+                .post("/users")
+                .then()
+                .log().body()
+                .statusCode(201)
+                .body("name", equalTo(firstName));
+
+    }
+
+    @Title("Getting User from the record")
+    @Test
+    public void test002() {
+
+        UsersPojo usersPojo = new UsersPojo();
+        usersPojo.setName(firstName = "Michael");
+
+        SerenityRest.rest()
+                .header("Content-Type", "application/json")
+                .given()
+                .when()
+                .get("/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("data.first_name", hasItems(firstName));
+
+    }
+
+    @Title("This test will update details of an user with ID5")
+    @Test
+    public void test003() {
+        UsersPojo usersPojo = new UsersPojo();
+        usersPojo.setName(firstName = "Johny");
+        usersPojo.setJob(job = "Golf");
+
+        SerenityRest.rest().given()
+                .log().all()
+                .header("Content-Type", "application/json")
+                .when().log().body()
+                .body(usersPojo)
+                .put("/5")
+                .then()
+                .statusCode(200).log().body()
+                .body("name", equalTo(firstName),
+                        "job", equalTo(job));
+
+    }
+
+    @Title("This test will delete an User")
+    @Test
+    public void test004() {
+        SerenityRest.rest().given()
+                .when()
+                .delete("/users/2")
+                .then().statusCode(204)
+                .log().status()
+                .log().ifValidationFails();
+
+    }
+
+
+
+
+
+}
